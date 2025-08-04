99d8ef2a5fc6d44c3ab37fc2168eb37f1949ba7d
Vishal <statusb130@gmail.com>
2025-08-04 22:21:57 +0530

    Bottom App Bar

---
+++ app/src/main/java/com/rach/masterxml/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.rach.masterxml
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val fragmentBindier = findViewById<BottomNavigationView>(R.id.bottomNavigationBar)
+        val navController = findNavController(R.id.fragmenthost)
+        fragmentBindier.setupWithNavController(navController)
+    }
+}
